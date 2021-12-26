1750b9478b667a519c1a5d0ccc154d1798e32084
stevejtrettel <stevejtrettel@gmail.com>
2021-12-26 12:04:20 -0800

    renderer and camera

--- common/components/createCamera.js
+++ common/components/createCamera.js
@@ -1,9 +1,47 @@
+import {
+    PerspectiveCamera,
+    DirectionalLight,
+} from "../../3party/three/build/three.module.js";
 
 
+function createCamera() {
+    const camera = new PerspectiveCamera(
+        35, // fov = Field Of View
+        1, // aspect ratio (dummy value)
+        0.1, // near clipping plane
+        200, // far clipping plane
+    );
 
-function createCamera () {
+    // move the camera back so we can view the scene
+    camera.position.set(0, 8, 10);
 
+    //animation for camera:
+    camera.tick = () => {
+        //default = do nothing here
+        // camera.fov += 0.5;
+        // camera.updateProjectionMatrix();
+    };
+
+    return camera;
 }
 
 
-export { createCamera };
+
+function createCameraLight() {
+
+    //const cameraLight = new PointLight('white', 5);
+    const cameraLight = new DirectionalLight('white', 1);
+
+
+    //move light directly behind us
+    //this position will be relative to the camera's frame
+    cameraLight.position.set(0.5,0.5,0.5);
+
+    //cameraLight.castShadow=true;
+
+    return cameraLight;
+}
+
+
+
+export { createCamera, createCameraLight };
--- common/components/createRenderer.js
+++ common/components/createRenderer.js
@@ -1,7 +1,28 @@
-
+import {
+    WebGLRenderer,
+    sRGBEncoding,
+    CineonToneMapping,
+    ACESFilmicToneMapping,
+} from "../../3party/three/build/three.module.js";
 
 
 function createRenderer ( ) {
+
+    const renderer = new WebGLRenderer({
+        antialias : true,
+        preserveDrawingBuffer: true,
+        powerPreference: "high-performance",
+    });
+
+    // turn on the physically correct lighting model
+    renderer.physicallyCorrectLights = true;
+
+    //tone to screen
+    renderer.toneMapping = CineonToneMapping;
+    //ACESFilmicToneMapping;
+    renderer.outputEncoding = sRGBEncoding;
+
+    return renderer;
 
 }
 
