0afd0e338833fbb89de6366da5252ffed0d02646
Steve Trettel <stevejtrettel@gmail.com>
2022-11-26 22:51:57 -0800

    working cylinder eigenfunctions

--- common/items/SphericalHarmonics.js
+++ common/items/SphericalHarmonics.js
@@ -1,9 +1,7 @@
-import SphericalHarmonic from "../shaders/math/sphericalHarmonic.js";
+
 import { colorConversion } from "../shaders/colors/colorConversion.js";
 import { ParametricMaterial } from "../materials/ParametricMaterial.js";
 import sphericalHarmonic from "../shaders/math/sphericalHarmonic.js";
-
-
 
 
 let uniforms = {
