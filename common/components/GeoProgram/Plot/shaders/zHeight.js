242de8fafb7d0b3d4709b03eadaf424a84fb117f
Steve Trettel <stevejtrettel@gmail.com>
2023-10-18 11:10:54 -0700

    zheight update

--- common/components/GeoProgram/Plot/shaders/zHeight.js
+++ common/components/GeoProgram/Plot/shaders/zHeight.js
@@ -1,13 +1,21 @@
+import {colorConversion} from "./utils/colorConversion.js";
 
-const zHeight = `
+
+
+const zHeight = colorConversion + `
 vec3 colorFn(vec2 uv,vec3 xyz){
+
+        //allowable variables to use in coloring:
         float u = uv.x;
         float v = uv.y;
         float x = xyz.x;
         float y = xyz.y;
         float z = xyz.z;
         
-        return vec3(0.2,0.3,y);
+        float height = -0.5*tanh(2.*y)+0.3;
+        vec3 col = hsb2rgb(vec3(height,0.4,0.5));
+        
+        return col;
     }
 `;
 
